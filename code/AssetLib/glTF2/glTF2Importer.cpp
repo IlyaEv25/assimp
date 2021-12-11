a8fff6e97f143d34d1164dcae7dfd07e4324ca52
Ilya Evteev <evteevil6@gmail.com>
2021-12-11 14:59:34 +0500

    new1

--- code/AssetLib/glTF2/glTF2Importer.cpp
+++ code/AssetLib/glTF2/glTF2Importer.cpp
@@ -39,7 +39,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ----------------------------------------------------------------------
 */
 
-//new shit
+//new
 #if !defined(ASSIMP_BUILD_NO_GLTF_IMPORTER) && !defined(ASSIMP_BUILD_NO_GLTF2_IMPORTER)
 
 #include "AssetLib/glTF2/glTF2Importer.h"
